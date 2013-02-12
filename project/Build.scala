bc12361f5efad6d445e018268d4374560c2c3730
Patrick Lightbody <patrick@lightbody.net>
2013-02-11 22:22:08 -0800

    ready for deploy to heroku!

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "postgresql" % "postgresql" % "9.1-902.jdbc4"
+        "postgresql" % "postgresql" % "9.1-901.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
